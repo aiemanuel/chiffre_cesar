c8e78a5c31f9895cadf962bdeaea580d3c246ff6
aiemanuel <74431454+aiemanuel@users.noreply.github.com>
2021-02-12 10:58:25 -1000

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ while True:
         msg_chiffre =chiffrement (msg)
         print(f"le message chiffre est {msg_chiffre}")
         break
+    elif choix == "d":
+        msg = input("Entrez le message à déchiffrer (en lettres capitales, sans espace ni ponctuation) : ")
+        msg_clair = dechiffrement(msg)
+        print(f"Le message en clair est {msg_clair}.")
+        break
+    else:
+        print("Recommencez s'il vous plaît.")
